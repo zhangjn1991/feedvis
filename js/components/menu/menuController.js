bdc9c0f079027da36dce34d26c966581456c60a1
zhangjn1991 <zhangjn.uiuc@gmail.com>
2015-02-10 10:46:40 -0600

    Add set menu visible function

--- js/components/menu/menuController.js
+++ js/components/menu/menuController.js
@@ -2,8 +2,11 @@ angular.module('feedVisApp')
 .controller('MenuCtrl', function(){
 
 	this.isMenuVisible = false;
-	this.toggleMenuVisible = function(){
-		this.isMenuVisible = !this.isMenuVisible;
+	this.toggleMenuVisible = function(visible){
+		if(visible === undefined)
+			this.isMenuVisible = !this.isMenuVisible;
+		else
+			this.isMenuVisible = visible;
 	}
 });
 
