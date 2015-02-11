5a8df469cd91e63bc4a32afa5fac0fedde130ce8
zhangjn1991 <zhangjn.uiuc@gmail.com>
2015-02-10 23:08:11 -0600

    Add routing

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-angular.module('feedVisApp',[]);
+angular.module('feedVisApp',['ngRoute'])
+.config(['$routeProvider',function($routeProvider){
+	$routeProvider.
+		when('/', {
+			redirectTo:'/graph'
+		}).
+		when('/graph', {
+			templateUrl:'partials/graph-view.html'
+		}).
+		when('/goal', {
+			templateUrl:'partials/goal-view.html'
+		});
+}])
+
